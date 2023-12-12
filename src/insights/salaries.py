e90e4f16a223036d268202a3c7ddedbfbadbe45a
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2023-12-12 20:56:00 -0300

    Feat: req 07, flake 8 verificado

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -17,7 +17,14 @@ class ProcessSalaries(ProcessJobs):
         return max_salary
 
     def get_min_salary(self) -> int:
-        pass
+        salaries = []
+        for job in self.jobs_list:
+            min_salary = job.get("min_salary")
+            if min_salary.isdigit():
+                salaries.append(int(min_salary))
+
+        min_salary = min(salaries)
+        return min_salary
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
         pass
